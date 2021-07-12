2be84d30af02d46be76dbf252e67bc8b2dfc44a3
Lolo <mjmorales.mcv@gmail.com>
2021-07-12 14:44:17 +0200

    Started main, adds makefile and bool libray(pending to complete)

---
+++ main.c
@@ -0,0 +1,17 @@
+#ifndef MAIN
+#define MAIN main
+
+#pragma region INCLUDES
+#include <stdio.h>
+
+#pragma endregion
+
+#pragma region MAIN_CODE
+
+int main(){
+    printf("%s","Hola mundo cruel!");
+    return 0;
+}
+
+#pragma endregion
+#endif
