344275d716e997c61e238e5be00616143b6b7538
Lolo <mjmorales.mcv@gmail.com>
2021-07-12 15:04:31 +0200

    adds bool definition and Util macros

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 
 #pragma region INCLUDES
 #include <stdio.h>
-
+#define print printf
+#include "bool.h"
 #pragma endregion
 
 #pragma region MAIN_CODE
 
 int main(){
-    printf("%s","Hola mundo cruel!");
+
+    print("esta linea");print("esta otra linea esta abajo");
+
     return 0;
 }
 
